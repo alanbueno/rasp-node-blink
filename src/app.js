9fbb93d6c5e94cbb2705db6fc0e6fcf304018385
Alan Bueno <alanbueno8@gmail.com>
2018-07-11 23:33:19 -0300

    error treatment

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@ const Koa = require('koa')
 const app = new Koa()
 const bodyParser = require('koa-bodyparser')
 
+require('./middleware/sentry-logs')(app)
+
 app.use(bodyParser({ enableTypes: 'json' }))
 
 app.use(require('./middleware/response-header'))
 
 app.use(require('./middleware/error-handler'))
-require('./middleware/sentry-logs')(app)
+
+app.use(require('./middleware/response-decorator'));
 
 require('./routes')(app)
 
--- src/resources/toggleRelay/toggleRelay.controller.js
+++ src/resources/toggleRelay/toggleRelay.controller.js
@@ -1,24 +1,25 @@
 // @flow
 
 const { Bus, Device } = require('i2c-bus-promised')
+const { to } = require('await-to-js')
 
-const relayBanks = {a: 0x12, b: 0x13}
+const relayBanks = { a: 0x12, b: 0x13 }
 
 const relays = [
-  {iRelay: 1, iDic: 0, bank: relayBanks.a},
-  {iRelay: 2, iDic: 1, bank: relayBanks.a},
-  {iRelay: 3, iDic: 2, bank: relayBanks.a},
-  {iRelay: 4, iDic: 3, bank: relayBanks.a},
-  {iRelay: 5, iDic: 4, bank: relayBanks.a},
-  {iRelay: 6, iDic: 5, bank: relayBanks.a},
-  {iRelay: 7, iDic: 6, bank: relayBanks.a},
-  {iRelay: 8, iDic: 7, bank: relayBanks.a},
-  {iRelay: 9, iDic: 0, bank: relayBanks.b},
-  {iRelay: 10, iDic: 1, bank: relayBanks.b},
+  { iRelay: 1, iDic: 0, bank: relayBanks.a },
+  { iRelay: 2, iDic: 1, bank: relayBanks.a },
+  { iRelay: 3, iDic: 2, bank: relayBanks.a },
+  { iRelay: 4, iDic: 3, bank: relayBanks.a },
+  { iRelay: 5, iDic: 4, bank: relayBanks.a },
+  { iRelay: 6, iDic: 5, bank: relayBanks.a },
+  { iRelay: 7, iDic: 6, bank: relayBanks.a },
+  { iRelay: 8, iDic: 7, bank: relayBanks.a },
+  { iRelay: 9, iDic: 0, bank: relayBanks.b },
+  { iRelay: 10, iDic: 1, bank: relayBanks.b },
 ]
 const address = 0x20
 
-async function toggleRelay (ctx) {
+async function toggleRelay(ctx) {
   const bus = new Bus();
   await bus.open();
 
@@ -26,31 +27,28 @@ async function toggleRelay (ctx) {
 
   let actualRelay = relays.find(relay => relay.iRelay === Number(ctx.params.idRelay))
 
-  await relaysBus.writeByte(0x00,0x00);
-  await relaysBus.writeByte(0x01,0x00);
+  await relaysBus.writeByte(0x00, 0x00);
+  await relaysBus.writeByte(0x01, 0x00);
 
-  Promise.all([
-    relaysBus.readWord(actualRelay.bank)
-  ])
-    .then(async ([value]) => {
-      //Shift the bits for the register value, checking if they are already set first
-      if ((value >> actualRelay.iDic) & 1) {
-        //already high go to lo low state
-        value -= (1 << actualRelay.iDic)
-      } else {
-        //already lo go to high low state
-        value += (1 << actualRelay.iDic)
-      }
+  let [err, word] = await to(relaysBus.readWord(actualRelay.bank))
+  if (err) {
+    throw err
+  }
 
-      await relaysBus.writeByte(actualRelay.bank,value);
+  if ((value >> actualRelay.iDic) & 1) {
+    //already high go to lo low state
+    value -= (1 << actualRelay.iDic)
+  } else {
+    //already lo go to high low state
+    value += (1 << actualRelay.iDic)
+  }
 
-      return ctx.body = {
-        state: value
-      }
-    })
-    .catch(err => {
-      console.log(err)
-    })
+  await relaysBus.writeByte(actualRelay.bank, value);
+
+  // ctx.status = 200 
+  return ctx.body = {
+    state: value
+  }
 }
 
 module.exports.toggleRelay = toggleRelay
