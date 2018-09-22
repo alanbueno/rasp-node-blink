723460b82868b9f17427176c59e87a5ea4d398c8
Alan Bueno <alanbueno8@gmail.com>
2018-09-22 18:40:17 -0300

    isolating i2c test

--- src/middleware/i2c-bus.js
+++ src/middleware/i2c-bus.js
@@ -2,12 +2,12 @@ const { Bus, Device } = require('i2c-bus-promised')
 const address = 0x20
 
 
-module.exports = async (ctx, next) => {
+module.exports = async () => {
   try {
     const bus = new Bus()
     await bus.open()
 
-    const i2cBus = new Device(bus, address)
+    const i2cBus = await new Device(bus, address)
 
     await i2cBus.writeByte(0x00, 0x00)
     await i2cBus.writeByte(0x01, 0x00)
